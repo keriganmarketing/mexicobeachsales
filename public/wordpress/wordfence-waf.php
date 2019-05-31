9776a37804cc8193a6e1255a1adcef5fe194fe39
Bryan Baird <bryan@kerigan.com>
2019-05-31 09:42:09 -0400

    add waf file back

---
+++ public/wordpress/wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('C:\\dev\\mexicobeachsales\\public\\plugins\\wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", 'C:\\dev\\mexicobeachsales\\public/wflogs/');
+	include_once 'C:\\dev\\mexicobeachsales\\public\\plugins\\wordfence/waf/bootstrap.php';
+}
+?>

a4aca9d0fd9acea0d04419fc03e57a9e9783054d
ToniSHernandez <toni@kerigan.com>
2021-08-12 17:53:22 -0500

    Added Sitemap to footer, kma open in new tab

--- public/themes/wordplate/views/partials/footer.blade.php
+++ public/themes/wordplate/views/partials/footer.blade.php
@@ -26,8 +26,10 @@
 
     </div>
     <hr>
-    <p class="copyright text-center">&copy;{{ date('Y') }} {{ get_bloginfo() }}. All Rights&nbsp;Reserved. 
-        <a style="text-decoration:underline;" href="/privacy-policy/" >Privacy&nbsp;Policy</a> 
+    <p class="copyright text-center">&copy;{{ date('Y') }} {{ get_bloginfo() }}. All Rights&nbsp;Reserved.&nbsp;
+        <a style="text-decoration:underline;" href="/privacy-policy/" >Privacy&nbsp;Policy</a>
+        &nbsp;|&nbsp;
+        <a style="text-decoration:underline;" href="/sitemap_index.xml" >Sitemap</a>
         <span class="siteby">
             <svg version="1.1" id="kma" xmlns="http://www.w3.org/2000/svg"
                 xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="14" width="20"
@@ -35,6 +37,6 @@
                 xml:space="preserve">
                     <path fill="#b4be35"
                 d="M6.4,0.1c0,0,0.1,0.3,0.2,0.9c1,3,3,5.6,5.7,7.2l-0.1,0.5c0,0-0.4-0.2-1-0.4C7.7,7,3.7,7,0.2,8.5L0.1,8.1 c2.8-1.5,4.8-4.2,5.7-7.2C6,0.4,6.1,0.1,6.1,0.1H6.4L6.4,0.1z"></path>
-            </svg> &nbsp;<a href="https://keriganmarketing.com">Site&nbsp;by&nbsp;KMA</a>.
+            </svg> &nbsp;<a href="https://keriganmarketing.com" target="_blank" rel="noreferrer">Site&nbsp;by&nbsp;KMA</a>.
         </span></p>
 </footer>
